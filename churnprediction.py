2239ea3264e3fc71f2a3a008901e9ccf37b9ca1f
subhankar453 <50748677+subhankar453@users.noreply.github.com>
2020-04-07 23:15:50 +0530

    Adding layers of ANN
    Building the network of ANN after data pre-processing

--- churnprediction.py
+++ churnprediction.py
@@ -7,18 +7,18 @@ import matplotlib.pyplot as plt
 
 #Step-2 Importing dataset and finding the input and output variables
 dataset = pd.read_csv('Churn_Modelling.csv')
-x = dataset.iloc[:, 3:12].values
+x = dataset.iloc[:, 3:13].values
 y = dataset.iloc[:, 13].values
 
 #Step-3 Encoding the categorical data in the dataset for ANN
-from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-labelEncoder_X1 = LabelEncoder()
-labelEncoder_X2 = LabelEncoder()
-x[:, 1] = labelEncoder_X1.fit_transform(x[:, 1])
-y[:, 2] = labelEncoder_X2.fit_transform(x[:, 2])
-oneHotEncoder = OneHotEncoder(categorical_features = [1])
-x = oneHotEncoder.fit_transform(x).toarray()
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+ct = ColumnTransformer([("Geography", OneHotEncoder(), [1])], remainder = 'passthrough')
+x = ct.fit_transform(x);
 x = x[:, 1:]
+from sklearn import preprocessing
+labelencoder = preprocessing.LabelEncoder()
+x[:, 3] = labelencoder.fit_transform(x[:, 3])
 
 #Step-4 Splitting the dataset into training set and test set
 from sklearn.model_selection import train_test_split
@@ -30,4 +30,24 @@ sc = StandardScaler
 x_train = sc.fit_transform(x_train)
 x_test = sc.fit_transform(x_test)
 
+#Part-2 Building the ANN network
+
+#Step-1 Importing keras library. Then Sequential function is imported to initialize the ANN model
+#The Dense function is imported to create the layers in ANN network
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+
+#Step-2 Developing the network layers
+classifier = Sequential()
+#Creating the input layer and the first hidden layer
+classifier.add(Dense(units = 6, activation = 'relu', kernel_initializer='uniform', input_dim = 11))
+#Creating the second hidden layer
+classifier.add(Dense(units = 6, activation = 'relu', kernel_initializer='uniform'))
+#Creating the output layer
+classifier.add(Dense(units = 1, activation = 'sigmoid', kernel_initializer='uniform'))
+#Compiling the ANN
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', weighted_metrics = ['accuracy'])
+#Fitting the ANN to the training set
+classifier.fit(x_train, y_train, batch_size = 10, epochs = 100)
 
